e4bb43945b88b67fc1b6bb1a34605c6f79f30201
Eyetu Kingsley <eyetukingsley330@gmail.com>
2024-07-01 01:19:00 +0100

    I have setup the application, added the main font and main color.

---
+++ src/app/chakra-theme.ts
@@ -0,0 +1,13 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  fonts: {
+    heading: "var(--font-dm-sans)",
+    body: "var(--font-dm-sans)",
+  },
+  colors: {
+    brand: {
+      100: "#405FF2",
+    },
+  },
+});
